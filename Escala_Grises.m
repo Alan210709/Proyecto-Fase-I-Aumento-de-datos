7ade91fde0a31c03fb6a1ce176b2bf3cf0c5425a
Alan210709 <alan_artemio@outlook.com>
2026-02-05 19:41:46 -0600

    Delete Escala_Grises.m

--- Escala_Grises.m
+++
@@ -1,9 +0,0 @@
-%Transformación a escala de grises
-
-clear; clc;
-Im = imread('C:\Users\Alan_\OneDrive\Imágenes\Álbum de cámara\ss_11942c933c595bf0188d5ee22f6f6a7ed3cdf7d9.1920x1080.jpg');
-figure; imshow(Im);
-Im = double(Im);
-Im = 0.2989*Im(:,:,1) + 0.5870*Im(:,:,2) + 0.1140*Im(:,:,3);
-Im = uint8(Im);
-figure; imshow(Im);
